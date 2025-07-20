5343a8d6a102203396e3cd572162202757af632a
Shao49 <Futelvn@gmail.com>
2025-07-20 15:55:05 +0300

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,19 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+
+        {/* Google Analytics script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R7Y0H1T300"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R7Y0H1T300');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
